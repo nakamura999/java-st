cf91612ecad44c4cf5fa873ea7d7a04443dfc4a7
nakamura999 <masahiro9ama9@gmail.com>
2020-07-13 12:38:57 +0900

    first commit

---
+++ java-kiso/just.java
@@ -0,0 +1,9 @@
+public class just {
+    public static void main(String[] args) {
+        System.out.println("田中");
+        System.out.println("30歳です");
+        System.out.println("日本酒が好きです");
+        System.out.println("30+30の計算をします");
+        System.out.println(30 + 30);
+    }
+}
