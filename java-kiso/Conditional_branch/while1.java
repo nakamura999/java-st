cf2c27d9e667b2a794ea58743c1e0899bccead1b
nakamura999 <masahiro9ama9@gmail.com>
2020-07-14 17:29:54 +0900

    Conditional_branch add

---
+++ java-kiso/Conditional_branch/while1.java
@@ -0,0 +1,9 @@
+public class while1 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		while (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		}
+	}
+}
