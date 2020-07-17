e0a267f5ac2cc659e1dea96c339b4c0e07edd120
nakamura999 <masahiro9ama9@gmail.com>
2020-07-17 14:38:15 +0900

    hairetu

---
+++ hairetu/src/hairetu/hairetu1.java
@@ -0,0 +1,12 @@
+package hairetu;
+
+public class hairetu1 {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3};
+		int[] b;
+		b = a;
+		b[0] = 100;
+		System.out.println(a[0]);
+	}
+
+}
