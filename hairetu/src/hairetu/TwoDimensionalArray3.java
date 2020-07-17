f8866057a7755b66fa5b121cbf17b6bdc6998eb5
nakamura999 <masahiro9ama9@gmail.com>
2020-07-17 15:35:50 +0900

    method add

---
+++ hairetu/src/hairetu/TwoDimensionalArray3.java
@@ -0,0 +1,14 @@
+package hairetu;
+
+public class TwoDimensionalArray3 {
+	public static void main(String[] args) {
+		int[] moneyList = {121111, 70443, 223010};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+
+}
---
+++ method/src/method/hello.java
@@ -0,0 +1,12 @@
+package method;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("メソッドの呼び出しが終わりました");
+		hellos();
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+	public static void hellos() {
+		System.out.println("こんにちは");
+	}
+}
