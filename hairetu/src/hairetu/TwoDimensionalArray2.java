ac797c81443c1ad9a715082c066c5446f59718e4
nakamura999 <masahiro9ama9@gmail.com>
2020-07-17 15:13:05 +0900

    hairetu edit

---
+++ hairetu/src/hairetu/TwoDimensionalArray2.java
@@ -0,0 +1,11 @@
+package hairetu;
+
+public class TwoDimensionalArray2 {
+	public static void main(String[] args) {
+		int[][] scores = {{10, 20, 30}, {30, 40, 50}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+	}
+
+}
+// 親配列と子配列の要素数を表示
---
+++ hairetu/src/hairetu/nul1.java
@@ -0,0 +1,11 @@
+package hairetu;
+
+public class nul1 {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3};
+		a = null;
+		a[0] =10;
+	}
+
+}
+// nullを代入することで参照をきる
