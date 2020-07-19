66f73cbce10071ebad17d780933907efb1220d4c
nakamura999 <masahiro9ama9@gmail.com>
2020-07-19 15:24:20 +0900

    method edit

---
+++ method/src/method/Value1.java
@@ -0,0 +1,11 @@
+package method;
+
+public class Value1 {
+	public static void main(String[] args) {
+		int[] score = {20, 30, 40, 50, 80};
+		for (int value : score) {
+			System.out.println(value);
+		}
+	}
+
+}
