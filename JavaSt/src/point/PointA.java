8795085cfedb654871f09b8c9232512e117bbd31
nakamura999 <masahiro9ama9@gmail.com>
2020-07-20 13:59:13 +0900

    point edit

---
+++ JavaSt/src/point/PointA.java
@@ -0,0 +1,26 @@
+package point;
+
+public class PointA {
+	String name;
+	int hp;
+	void sleep() {
+		this.hp = 100;
+		System.out.println(this.name + "は、眠って回復したい。");
+	}
+	void sit(int sec) {
+		this.hp += sec;
+		System.out.println(this.name + "は、" + sec + "秒座った");
+		System.out.println("HPが" + sec + "ポイント回復した");
+	}
+	void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んだ！");
+		System.out.println("5のダメージ！");
+	}
+	void run() {
+		System.out.println(this.name + "は逃げ出した");
+		System.out.println("GAMEOVER");
+		System.out.println("最終HPは" + this.hp + "でした");
+	}
+
+}
