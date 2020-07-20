f9f139d49265ecba8529050962469282dacf8daa
nakamura999 <masahiro9ama9@gmail.com>
2020-07-20 15:55:33 +0900

    hello5 edit

---
+++ JavaSt/src/hello5/Hero.java
@@ -0,0 +1,10 @@
+package hello5;
+
+public class Hero {
+	String name;
+	int hp;
+	Hero(String name) {
+		this.hp = 100;
+		this .name = name;
+	}
+}
---
+++ JavaSt/src/hello5/Main.java
@@ -0,0 +1,10 @@
+package hello5;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h = new Hero("サトシ");
+
+		System.out.println(h.hp);
+		System.out.println(h.name);
+	}
+}
