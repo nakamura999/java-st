c4a7a33b3d497131f2deabc70e2969bc2976bc04
nakamura999 <masahiro9ama9@gmail.com>
2020-07-20 16:08:28 +0900

    hello6 edit

---
+++ JavaSt/src/hello6/Hero.java
@@ -0,0 +1,14 @@
+package hello6;
+
+public class Hero {
+	String name;
+	int hp;
+	Hero(String name) {
+		this.hp = 100;
+		this.name = name;
+	}
+	Hero() {
+		this.hp = 100;
+		this.name = "ダミー";
+	}
+}
---
+++ JavaSt/src/hello6/Main.java
@@ -0,0 +1,13 @@
+package hello6;
+
+public class Main {
+	public static void main(String[] args) {
+		Hero h1 = new Hero("サトシ");
+
+		System.out.println(h1.name);
+		Hero h2 = new Hero();
+
+		System.out.println(h2.name);
+	}
+
+}
