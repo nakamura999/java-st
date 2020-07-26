7cb9ac26886379204f1c619400873a52f5a48969
nakamura999 <masahiro9ama9@gmail.com>
2020-07-26 17:13:56 +0900

    ST1 edit

---
+++ ST1/src/st6/Main.java
@@ -0,0 +1,12 @@
+package st6;
+
+public class Main {
+	public static void main(String[] args) {
+		int length = 6;
+		int height = 8;
+		int rectangleArea = length * height;
+		System.out.println(rectangleArea);
+		int triangleArea = length * height / 2;
+		System.out.println(triangleArea);
+	}
+}
---
+++ ST1/src/st7/Main.java
@@ -0,0 +1,10 @@
+package st7;
+
+public class Main {
+	public static void main(String[] args) {
+		double number1 = 8.3;
+		double number2 = 39.9;
+		System.out.println(number1 + number2);
+		System.out.println(number1 - number2);
+	}
+}
