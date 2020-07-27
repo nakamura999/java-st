33bc4749c846e05ea0e24319584566dd2711776a
nakamura999 <masahiro9ama9@gmail.com>
2020-07-27 12:25:23 +0900

    ST2 add

---
+++ ST2/src/st1/Main.java
@@ -0,0 +1,14 @@
+// 真偽値・比較演算子
+package st1;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println(false);
+		System.out.println(12/4 == 3);
+		System.out.println(12/4 != 3);
+		boolean bool = (3*9 ==27);
+		System.out.println(bool);
+	}
+
+}
