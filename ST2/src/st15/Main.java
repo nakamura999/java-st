1c6f19657085b31d3a0df6fb620315bbc46a5ecd
nakamura999 <masahiro9ama9@gmail.com>
2020-07-28 16:41:01 +0900

    ST2 st15 edit

---
+++ ST2/src/st15/Main.java
@@ -0,0 +1,30 @@
+package st15;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] numbers = {1, 4, 6, 9, 13, 16};
+
+		int oddSum = 0;
+		int evenSum = 0;
+
+		for (int number: numbers) {
+			if (number % 2 == 0) {
+				evenSum += number;
+			} else {
+				oddSum += number;
+			}
+		}
+
+		System.out.println("奇数の和は" + oddSum + "です");
+		System.out.println("偶数の和は" + evenSum + "です");
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 == 0) {
+				evenSum += numbers[i];
+			} else {
+				oddSum += numbers[i];
+			}
+		}
+	}
+
+}
