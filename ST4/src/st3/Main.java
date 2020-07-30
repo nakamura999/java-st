fffe93c3b638382e466df3362c936d18fd5547bf
nakamura999 <masahiro9ama9@gmail.com>
2020-07-30 14:16:08 +0900

    ST4 edit

---
+++ ST4/src/st3/Main.java
@@ -0,0 +1,19 @@
+package st3;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("名前: ");
+		String firstName = scanner.next();
+		System.out.print("名字: ");
+		String lastName = scanner.next();
+		System.out.println("名前は" + fullName(firstName, lastName) + "です");
+	}
+
+	public static String fullName(String firstName, String lastName) {
+		return firstName + " " + lastName;
+	}
+
+}
