8658f3cb1e6afa16782e481a628de7fe315f7702
nakamura999 <masahiro9ama9@gmail.com>
2020-07-30 16:17:21 +0900

    ST5 add

---
+++ ST5/src/st1/Main.java
@@ -0,0 +1,13 @@
+// オブジェクト指向
+package st1;
+
+public class Main {
+	public static void main(String[] args) {
+		Person person1 = new Person("田中　勇気");
+		Person person2 = new Person("佐藤　健");
+
+		person1.hello();
+		person2.hello();
+	}
+
+}
---
+++ ST5/src/st1/Person.java
@@ -0,0 +1,14 @@
+package st1;
+
+public class Person {
+	public String name;
+
+	Person(String name) {
+		this.name = name;
+	}
+
+	public void hello() {
+		System.out.println("こんにちは、私は" + this.name + "です");
+	}
+
+}
