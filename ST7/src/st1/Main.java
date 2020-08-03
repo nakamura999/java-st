4331a1c6a767b568f39e2edacaf7c25073e8c57d
nakamura999 <masahiro9ama9@gmail.com>
2020-08-03 15:34:42 +0900

    ST7 2 edit

--- ST7/src/st1/Main.java
+++ ST7/src/st1/Main.java
@@ -4,7 +4,19 @@ package st1;
 public class Main {
 	public static void main(String[] args) {
 		Car car = new Car();
+		car.setName("フェラーリ");
+		car.setColor("赤");
+
 		Bicycle bicycle = new Bicycle();
+		bicycle.setName("ビアンキ");
+		bicycle.setColor("緑");
+
+		System.out.println("[車の情報]");
+		car.printData();
+
+		System.out.println("===========");
+		System.out.println("[自動車の情報]");
+		bicycle.printData();
 	}
 
 }
