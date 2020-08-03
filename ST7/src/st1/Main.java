c93fe42797eb84992afecb0642301f223934d5d3
nakamura999 <masahiro9ama9@gmail.com>
2020-08-03 17:31:54 +0900

    ST7 4 edit

--- ST7/src/st1/Main.java
+++ ST7/src/st1/Main.java
@@ -1,8 +1,11 @@
 // 継承
 package st1;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		Car car = new Car();
 		car.setName("フェラーリ");
 		car.setColor("赤");
@@ -12,7 +15,12 @@ public class Main {
 		bicycle.setColor("緑");
 
 		System.out.println("[車の情報]");
-		car.printData();
+		System.out.println("ガソリンの量" + car.getFuel() + "L");
+		System.out.println("-------------");
+		System.out.println("給油する量を入力してください : ");
+		int litre = scanner.nextInt();
+		car.charge(litre);
+//		car.printData();
 
 		System.out.println("===========");
 		System.out.println("[自動車の情報]");
