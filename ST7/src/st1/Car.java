be53815a53d8408f5492083e71b724d730ffeea5
nakamura999 <masahiro9ama9@gmail.com>
2020-08-03 17:42:56 +0900

    ST7 6 edit

--- ST7/src/st1/Car.java
+++ ST7/src/st1/Car.java
@@ -8,9 +8,10 @@ public class  Car extends Vehicle {
 	}
 
 	public void printData() {
-		System.out.println("名前 : " + this.getName());
-		System.out.println("色 : " + this.getColor());
-		System.out.println("走行距離 : " + this.getDistance() + "km");
+//		System.out.println("名前 : " + this.getName());
+//		System.out.println("色 : " + this.getColor());
+//		System.out.println("走行距離 : " + this.getDistance() + "km");
+		super.printData();
 		System.out.println("ガソリン量 : " + this.getFuel() + "L");
 	}
 
