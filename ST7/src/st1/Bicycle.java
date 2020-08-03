d49766ed3d1f98e197c43184b80945560114b7f2
nakamura999 <masahiro9ama9@gmail.com>
2020-08-03 15:26:57 +0900

    ST7 add

---
+++ ST7/src/st1/Bicycle.java
@@ -0,0 +1,5 @@
+package st1;
+
+public class Bicycle extends Vehicle {
+
+}
---
+++ ST7/src/st1/Main.java
@@ -0,0 +1,10 @@
+// 継承
+package st1;
+
+public class Main {
+	public static void main(String[] args) {
+		Car car = new Car();
+		Bicycle bicycle = new Bicycle();
+	}
+
+}
