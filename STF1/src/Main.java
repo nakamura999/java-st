e62d622ec647771767791bcdcadf29ee7fa4891b
nakamura999 <masahiro9ama9@gmail.com>
2020-10-02 22:25:05 +0900

    review

---
+++ STF1/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		String[] names = {"田中", "佐々木", "加藤"};
+
+		for (String name: names) {
+			System.out.println("私の名前は" + name + "です");
+		}
+	}
+}
