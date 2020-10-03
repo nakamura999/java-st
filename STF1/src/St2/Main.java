f72eafd025e12a9d248fae4c1c009acf65d8bb95
nakamura999 <masahiro9ama9@gmail.com>
2020-10-03 14:34:01 +0900

    STF1 add to

---
+++ STF1/src/St2/Main.java
@@ -0,0 +1,13 @@
+package St2;
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 1;i <= 2; i++) {
+			for(int j = 1; j <= 3; j++) {
+				int k = i +j;
+				System.out.println(i + "+" + j + "=" + k + " ");
+			}
+			System.out.println();
+		}
+	}
+}
