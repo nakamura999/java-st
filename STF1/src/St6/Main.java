5b9c410f67fc023545b6dc7106d1728d33a1ccec
nakamura999 <masahiro9ama9@gmail.com>
2020-10-04 22:42:30 +0900

    St8 add

---
+++ STF1/src/St6/Main.java
@@ -0,0 +1,17 @@
+package St6;
+
+public class Main {
+	public static void main(String[] args) {
+		double one, two, three;
+		double sum, avg;
+		one = 1.2;
+		two = 3.7;
+		three = 4.1;
+		System.out.println(one + " " + two + " " + three);
+		sum = one + two + three; // 合計値
+		avg = sum / 3.0; // 平均値
+		System.out.println("合計値: " + sum);
+		System.out.println("平均値: " + avg);
+	}
+
+}
---
+++ STF1/src/St8/SampleClass01.java
@@ -0,0 +1,15 @@
+package St8;
+
+public class SampleClass01 {
+	int n = 10;
+	String s = "field";
+	int add(int a, int b) {
+		return a + b;
+	}
+	String add(String s) {
+		return this.s + s;
+	}
+	void showNum() {
+		System.out.println("n = " + n);
+	}
+}
