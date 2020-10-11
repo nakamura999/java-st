98fa6281d6f2eed2e9fef3a3cd8508cb0932c967
nakamura999 <masahiro9ama9@gmail.com>
2020-10-11 15:52:22 +0900

    Exercises add

---
+++ STF2/src/St6/Fighter.java
@@ -0,0 +1,11 @@
+package St6;
+
+public class Fighter {
+	public void fly() {
+		System.out.println("飛行する");
+	}
+	public void fight() {
+		System.out.println("戦闘する");
+	}
+
+}
---
+++ STF2/src/St6/Problem.java
@@ -0,0 +1,12 @@
+package St6;
+
+public class Problem {
+	public static void main(String[] args) {
+		Fighter f = new Fighter();
+		Airplane a = new Airplane();
+		a.fly();
+		f.fly();
+		f.fight();
+	}
+
+}
