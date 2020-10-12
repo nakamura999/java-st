ddf28996311ee800e6c65db7b66ebd11468a067c
nakamura999 <masahiro9ama9@gmail.com>
2020-10-12 18:12:46 +0900

    Exercises add

---
+++ STF2/src/St12/IEmail.java
@@ -0,0 +1,6 @@
+package St12;
+
+public interface IEmail {
+	// メールを送る
+	public void sendMail(String address);
+}
---
+++ STF2/src/St12/IPhone.java
@@ -0,0 +1,6 @@
+package St12;
+
+public interface IPhone {
+	// 指定した番号に電話をかける
+	public void call(String number);
+}
