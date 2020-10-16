d174c1da77b5f94171195a66462e3cba969752d3
nakamura999 <masahiro9ama9@gmail.com>
2020-10-16 21:35:02 +0900

    Exercises add

---
+++ STF2/src/st21/SampleClass.java
@@ -0,0 +1,31 @@
+package st21;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class SampleClass {
+    public static void main(String[] args) throws IOException{
+    	ArrayList<String> al = new ArrayList<String>();
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+            while(true){
+            	System.out.print("文字を入力 : ");
+            	String buf = br.readLine();
+                if (buf.equals("")) {
+                	// 空白の場合
+                    break;
+                }
+                al.add(buf);
+            }
+
+            for(int i = 0; i < al.size(); i++){
+            	String s = (String)al.get(i);
+                if (s.length() < 5 ) {
+                	System.out.print(s + " ");
+                }
+            }
+     }
+}
