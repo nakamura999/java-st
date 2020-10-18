65ba364a2fcaa6bfabde36af0e268781d1eb5441
nakamura999 <masahiro9ama9@gmail.com>
2020-10-18 18:36:23 +0900

    Exercises add

---
+++ STF2/src/st31/SampleClass.java
@@ -0,0 +1,13 @@
+package st31;
+
+public class SampleClass {
+	public static void main(String[] args) {
+        String s1 = "hoge";
+        String s2 = new String("hoge");
+        if(s1.equals(s2)){
+            System.out.println(s1 + "と" + s2 +"は、同じ文字列です。");
+        }else{
+            System.out.println(s1 + "と" + s2 +"は、同じ文字列ではありません。");
+        }
+    }
+}
